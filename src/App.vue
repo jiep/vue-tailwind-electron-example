4d94271a377a71f31aff994ade90c599d831fc03
José Ignacio Escribano <jiep12345@hotmail.com>
2019-01-07 16:54:57 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+  div.h-screen.w-screen.bg-blue(id="app")
+    .content
+      router-view/
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+  .home
+    h1 This is a home page
+    router-link(:to="{ name: 'about' }") About
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
